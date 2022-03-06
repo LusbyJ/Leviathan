20a8e99e04016e846a23c502d0bf83b575fd07a5
Ashley Krattiger <akrattiger@unm.edu>
2022-03-06 14:58:46 -0700

    Added missing folders from previous commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public SpawnEnemy[] spawnPoints;
+	public TMP_Text roundText;
+	public TMP_Text creditText;
+	public float round;
+	public float credits;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        round = 1;
+		credits = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roundText.text = "Round " + round;
+		creditText.text = "Credits: " + credits;
+    }
+	
+	private void updateRound(){ round++; }
+}
