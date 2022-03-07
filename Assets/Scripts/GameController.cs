45aaf974ecbb5b97590729d7ec9e1a6919f5ea33
Ashley Krattiger <akrattiger@unm.edu>
2022-03-06 19:17:37 -0700

    Made sure everything mentioned in last commit was actually added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,11 +12,15 @@ public class GameController : MonoBehaviour
 	public float round;
 	public float credits;
 	
+	private List<Enemy> currEnemies;
+	
 	// Start is called before the first frame update
     void Start()
     {
         round = 1;
 		credits = 5;
+		currEnemies = new List<Enemy>();
+		currEnemies.Add(spawnPoints[0].spawnEnemyType(0));
     }
 
     // Update is called once per frame
@@ -24,6 +28,12 @@ public class GameController : MonoBehaviour
     {
         roundText.text = "Round " + round;
 		creditText.text = "Credits: " + credits;
+		foreach(Enemy e in currEnemies){
+			if(e.isDead()){
+				currEnemies.Remove(e);
+				Destroy(e);
+			}
+		}
     }
 	
 	private void updateRound(){ round++; }
