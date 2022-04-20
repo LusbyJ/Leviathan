fc9a28e18c0e4f106114bb3e038d4ff7afc6a51f
Ashley Krattiger <akrattiger@unm.edu>
2022-04-19 19:13:50 -0600

    Adjusted preview UI position, added preliminary score board functionality

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,26 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
-    public int newScore;
-	public SortedList highScores;
+	public FloatSO ScoreSO;
+	public TMP_Text[] scoreTexts;
+	public TMP_Text[] nameTexts;
+	public int[] highScores;
+	public string[] scoreNames;
 	
-	// Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        highScores = new SortedList();
+        highScores = new int[10]{   PlayerPrefs.GetInt("1_Score"),
+									PlayerPrefs.GetInt("2_Score"),
+									PlayerPrefs.GetInt("3_Score"),
+									PlayerPrefs.GetInt("4_Score"),
+									PlayerPrefs.GetInt("5_Score"),
+									PlayerPrefs.GetInt("6_Score"),
+									PlayerPrefs.GetInt("7_Score"),
+									PlayerPrefs.GetInt("8_Score"),
+									PlayerPrefs.GetInt("9_Score"),
+									PlayerPrefs.GetInt("10_Score")
+		};
+		
+		scoreNames = new string[10]{PlayerPrefs.GetString("1_Name"),
+									PlayerPrefs.GetString("2_Name"),
+									PlayerPrefs.GetString("3_Name"),
+									PlayerPrefs.GetString("4_Name"),
+									PlayerPrefs.GetString("5_Name"),
+									PlayerPrefs.GetString("6_Name"),
+									PlayerPrefs.GetString("7_Name"),
+									PlayerPrefs.GetString("8_Name"),
+									PlayerPrefs.GetString("9_Name"),
+									PlayerPrefs.GetString("10_Name")
+		};		
+		updateScores();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < 10; i++){
+			scoreTexts[i].text = "" + highScores[i];
+			nameTexts[i].text = scoreNames[i];
+		}
     }
 	
-	// public void saveScore(){ 
-		// ScoreBoard.SetInt("newScore", newScore);
-		// ScoreBoard.Save(); 
-	// }
+	private void updateScores(){
+		bool added = false;
+		int oldScore = 0; 
+		int newScore = 0;
+		string oldName = "";
+		string newName = "";
+		for(int i = 0; i < 10; i++){
+			if(added){
+				newScore = oldScore;
+				newName = oldName;
+				oldScore = highScores[i];
+				oldName = scoreNames[i];
+				highScores[i] = newScore;
+				scoreNames[i] = newName;
+				PlayerPrefs.SetInt((i + "_Score"), newScore);
+				PlayerPrefs.SetString((i + "_Name"), newName);
+			} else{
+				if(ScoreSO.Value > highScores[i]){
+					oldScore = highScores[i];
+					oldName = scoreNames[i];
+					highScores[i] = (int)ScoreSO.Value;
+					scoreNames[i] = "";
+					PlayerPrefs.SetInt((i + "_Score"), (int)ScoreSO.Value);
+					PlayerPrefs.SetString((i + "_Name"), "");
+					added = true;
+				}
+			}
+		}
+		PlayerPrefs.Save();
+	}
 }
