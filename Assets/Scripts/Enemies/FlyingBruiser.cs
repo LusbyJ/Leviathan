00bfb5df48d1613b08c57e817edd2426fd6571d9
Ashley Krattiger <akrattiger@unm.edu>
2022-04-24 15:57:09 -0600

    Added AOE to FlyingBruiser

--- Assets/Scripts/Enemies/FlyingBruiser.cs
+++ Assets/Scripts/Enemies/FlyingBruiser.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class FlyingBruiser : Enemy
 {
-    // Start is called before the first frame update
+    public CircleCollider2D attackCollider;
+	
+	// Start is called before the first frame update
     void Start()
     {
         setGround(false);
 		setFlying(true);
+		attackCollider.enabled = false;
     }
 
     // Update is called once per frame
@@ -26,13 +29,25 @@ public class FlyingBruiser : Enemy
 		if (collision.gameObject.tag == "Tower" && moving)
 		{
 			moving = false;
-			collision.gameObject.GetComponent<Health>().takeDamage(power);
-			takeDamage(health);
+			StartCoroutine(attack());
 		}
 		if(collision.gameObject.tag == "Slime")
         {
 			Destroy(collision.gameObject);
 			poison();
         }
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.gameObject.tag == "Tower"){
+			collider.gameObject.GetComponent<Health>().takeDamage(power);
+		}
+	}
+	
+	IEnumerator attack(){
+		attackCollider.enabled = true;
+		yield return new WaitForSeconds(0.1f);
+		attackCollider.enabled = false;
+		takeDamage(health);
 	}
 }
